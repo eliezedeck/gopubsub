5a7d0a04338bcc39128c1d1dd970c9c438ba12bb
Elie Zedeck <elie.zedeck@gmail.com>
2016-01-04 18:39:40 +0300

    Don't export the PubSub to protect the API against stupid usage.

--- pubsub.go
+++ pubsub.go
@@ -2,9 +2,7 @@ package gopubsub
 
 import "log"
 
-// PubSub is the main struct for the implementation of this package. Use New()
-// instead of using this directly.
-type PubSub struct {
+type pubSub struct {
 	subscribers []chan interface{}
 
 	// Operations
@@ -23,11 +21,11 @@ type subscribeOp struct {
 	channel    chan interface{}
 }
 
-// New creates a pointer to a PubSub. PubSub uses a []chan interface{} to keep
+// New creates a pointer to a pubSub. pubSub uses a []chan interface{} to keep
 // the list of subscribers, and the `cap` parameter is the initial headroom, not
 // the final number of subscribers possible. It can go as high as you need to.
-func New(cap int) *PubSub {
-	ps := &PubSub{
+func New(cap int) *pubSub {
+	ps := &pubSub{
 		subscribers:     make([]chan interface{}, 0, cap),
 		newsub:          make(chan subscribeOp),
 		lastPublication: nil,
@@ -73,7 +71,7 @@ func New(cap int) *PubSub {
 				for _, suber := range ps.subscribers {
 					close(suber)
 				}
-				log.Println("PubSub closed!")
+				log.Println("pubSub closed!")
 				break
 			}
 		}
@@ -82,11 +80,11 @@ func New(cap int) *PubSub {
 	return ps
 }
 
-// Subscribe gets you subscribed to this PubSub channel and returns a chan on
+// Subscribe gets you subscribed to this pubSub channel and returns a chan on
 // which you can listen for updates. The `getinitial` is used to get the very
 // last entry on this channel prior to getting the subsequent updates.
-func (ps *PubSub) Subscribe(getinitial bool) chan<- interface{} {
-	// buffer set to one so that the PubSub loop will be able to send to this in
+func (ps *pubSub) Subscribe(getinitial bool) chan<- interface{} {
+	// buffer set to one so that the pubSub loop will be able to send to this in
 	// case of a getinitial == true without blocking
 	c := make(chan interface{}, 1)
 
@@ -101,7 +99,7 @@ func (ps *PubSub) Subscribe(getinitial bool) chan<- interface{} {
 
 // Publish is used to publish the given `data` to all subscribers. Not that the
 // `data` must not be nil or there will be panic.
-func (ps *PubSub) Publish(data interface{}) {
+func (ps *pubSub) Publish(data interface{}) {
 	if data == nil {
 		panic("Cannot Publish() nil. That is equivalent to a channel closure which is invalid.")
 	}
@@ -110,14 +108,14 @@ func (ps *PubSub) Publish(data interface{}) {
 
 // Unsubscribe removes the given channel `c` from the list of subscribers. Note
 // that `c` must have been returned by the Subscribe() API call.
-func (ps *PubSub) Unsubscribe(c chan interface{}) {
+func (ps *pubSub) Unsubscribe(c chan interface{}) {
 	ps.delsub <- c
 }
 
-// Close is used to dispose of the PubSub. After this call, all the channels
+// Close is used to dispose of the pubSub. After this call, all the channels
 // that have been distributed from the Subscribe() call will be closed and will
 // thus return the default nil value of the type.
-func (ps *PubSub) Close() {
+func (ps *pubSub) Close() {
 	if !ps.closed {
 		ps.closed = true
 		ps.close <- struct{}{}
