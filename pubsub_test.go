d632b0d84377cd28f79179184c9d2a6e9104195c
Elie Zedeck <elie.zedeck@gmail.com>
2016-01-04 21:44:14 +0300

    Add TestIncreasedSubscribers() and TestUnsubscribe()

--- pubsub_test.go
+++ pubsub_test.go
@@ -3,6 +3,7 @@ package gopubsub
 import (
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestBasicOps(t *testing.T) {
@@ -34,4 +35,31 @@ func TestBasicOps(t *testing.T) {
 
 	ps.Publish("another")
 	allGotAnother.Wait()
+}
+
+func TestIncreasedSubscribers(t *testing.T) {
+	ps := New(2)
+	defer ps.Close()
+
+	count := 5
+	for i := 0; i < count; i++ {
+		ps.Subscribe(false)
+	}
+
+	if len(ps.subscribers) != count {
+		t.Errorf("Expected to have %d subscribers, have %d", count, len(ps.subscribers))
+	}
+}
+
+func TestUnsubscribe(t *testing.T) {
+	ps := New(2)
+	defer ps.Close()
+
+	c := ps.Subscribe(false)
+	ps.Unsubscribe(c)
+	time.Sleep(10 * time.Millisecond) // wait for the op to complete
+
+	if len(ps.subscribers) > 0 {
+		t.Errorf("Expected to have 0 subscribers, have %d", len(ps.subscribers))
+	}
 }
