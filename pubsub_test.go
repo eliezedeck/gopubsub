6ecd8bed4fd97f180473d5db8011cc2da1e0ff7d
Elie Zedeck <elie.zedeck@gmail.com>
2016-01-04 20:19:30 +0300

    Remove verbosity from TestBasicOps()

--- pubsub_test.go
+++ pubsub_test.go
@@ -1,14 +1,13 @@
 package gopubsub
 
 import (
-	"log"
 	"sync"
 	"testing"
-	"time"
 )
 
 func TestBasicOps(t *testing.T) {
 	ps := New(4)
+	ps.Close()
 
 	ps.Publish("something")
 
@@ -17,19 +16,11 @@ func TestBasicOps(t *testing.T) {
 
 	test := func() {
 		c := ps.Subscribe(true)
-	L:
-		for {
-			select {
-			case d := <-c:
-				log.Println("Got data", d)
-				break L
-			case <-time.After(200 * time.Millisecond):
-				panic("timeout, didn't receive data")
-			}
-		}
 
+		<-c // getting "something" here
 		allGotSomething.Done()
-		log.Println("Got data", <-c)
+
+		<-c // getting "another" here
 		allGotAnother.Done()
 	}
 
@@ -40,9 +31,7 @@ func TestBasicOps(t *testing.T) {
 	}
 
 	allGotSomething.Wait()
+
 	ps.Publish("another")
 	allGotAnother.Wait()
-
-	ps.Close()
-	time.Sleep(100 * time.Millisecond)
 }
